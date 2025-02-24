46aefe566ad17a7898256adce97e43a6439c13b7
VICTOR GABRIEL SARTI MIRANDA <victor.gsmiranda@senacsp.edu.br>
2025-02-24 08:58:15 -0300

    feat: install spring

---
+++ src/main/java/br/senac/SpringPI/SpringPiApplication.java
@@ -0,0 +1,13 @@
+package br.senac.SpringPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPiApplication.class, args);
+	}
+
+}
