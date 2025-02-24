763262dac2a7a4a0bebc789d55601da3c5f055aa
victor-sarti <sartivictor24@gmail.com>
2025-02-24 10:33:41 -0300

    feat: config

---
+++ src/main/java/br/senac/SpringPI/Controller.java
@@ -0,0 +1,15 @@
+package br.senac.SpringPI;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping("/") //a fim de mapear a raiz do servidor
+    @ResponseBody //a fim de imprimir no corpo da página
+    public String Ola() {
+        return "Aplicação Funcionando";
+
+    }
+}
